5fd93fb9f724a6ee36a3ae7ea31ea4410ebfabad
Luis Fernando de Sousa Zarbielli <lfsz12@hotmail.com>
2026-05-19 23:26:40 -0300

    Portifolio Zarb

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -12,6 +12,11 @@ export function About() {
     offset: ['start end', 'center center'],
   })
 
+  const { scrollYProgress: rocketScrollProgress } = useScroll({
+    target: ref,
+    offset: ['start end', 'end start'],
+  })
+
   const line1X      = useTransform(scrollYProgress, [0, 0.55], [-500, 0])
   const line1Opacity = useTransform(scrollYProgress, [0, 0.45], [0, 1])
 
@@ -22,9 +27,9 @@ export function About() {
   const photoScale   = useTransform(scrollYProgress, [0.15, 0.65], [0.86, 1])
   const photoOpacity = useTransform(scrollYProgress, [0.15, 0.5], [0, 1])
 
-  const rocketY       = useTransform(scrollYProgress, [0, 0.5, 1], [360, 0, -420])
-  const rocketOpacity = useTransform(scrollYProgress, [0, 0.18, 0.78, 1], [0, 1, 1, 0])
-  const rocketScale   = useTransform(scrollYProgress, [0, 0.5, 1], [0.82, 1, 0.9])
+  const rocketTop     = useTransform(rocketScrollProgress, [0, 1], ['14%', '88%'])
+  const rocketOpacity = useTransform(rocketScrollProgress, [0, 0.06, 0.86, 1], [0.9, 1, 1, 0])
+  const rocketScale   = useTransform(rocketScrollProgress, [0, 0.18, 1], [0.92, 1, 0.9])
 
   const bioX       = useTransform(scrollYProgress, [0.3, 0.8], [-400, 0])
   const bioOpacity = useTransform(scrollYProgress, [0.3, 0.7], [0, 1])
@@ -47,14 +52,17 @@ export function About() {
         </motion.figure>
       </div>
 
-      <div className={styles.rocketAnchor}>
+      <motion.div
+        className={styles.rocketAnchor}
+        style={{ top: rocketTop, opacity: rocketOpacity }}
+      >
         <motion.div
           className={styles.rocketMotion}
-          style={{ y: rocketY, scale: rocketScale, opacity: rocketOpacity }}
+          style={{ scale: rocketScale }}
         >
           <RocketMan />
         </motion.div>
-      </div>
+      </motion.div>
 
       <motion.div className={styles.bio} style={{ x: bioX, opacity: bioOpacity }}>
         <p>
